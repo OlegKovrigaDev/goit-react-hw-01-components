5cef985e2d2d27cb5f05b6403968ab651c955491
Oleg Kovriga <olegkovrigaev@gmail.com>
2023-11-07 20:45:25 +0200

    Debug

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,13 +4,13 @@ import styles from './Statistics.module.scss';
 function Statistics({ title, stats }) {
   return (
     <section className={styles.statistics}>
-      <h2 className={styles.title}>{title && title}</h2>
+      {title && <h2 className={styles.title}>{title}</h2>}
 
       <ul className={styles['stat-list']}>
         {stats.map(item => (
           <li className={styles.item} key={item.id}>
             <span className={styles.label}>{item.label}</span>
-            <span className={styles.percentage}>{item.percentage}</span>
+            <span className={styles.percentage}>{item.percentage}%</span>
           </li>
         ))}
       </ul>
