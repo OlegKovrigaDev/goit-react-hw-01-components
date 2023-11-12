7d6cd2f6a117d311c52c5830cfc281658a0eddf9
Oleg Kovriga <olegkovrigaev@gmail.com>
2023-11-12 16:25:55 +0200

    Add bgColor

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import styles from './Statistics.module.scss';
 
+const getBgColor = variant => {
+  switch (variant) {
+    case 'info':
+      return 'blue';
+    case 'success':
+      return 'green';
+    case 'error':
+      return 'red';
+    case 'warning':
+      return 'orange';
+    case 'other':
+      return 'gray';
+    default:
+      throw new Error(`Unsupported variant prop value - ${variant}`);
+  }
+};
+
 function Statistics({ title, stats }) {
+  const colors = ['info', 'success', 'error', 'warning', 'other'];
   return (
     <section className={styles.statistics}>
       {title && <h2 className={styles.title}>{title}</h2>}
 
       <ul className={styles['stat-list']}>
-        {stats.map(item => (
-          <li className={styles.item} key={item.id}>
+        {stats.map((item, index) => (
+          <li
+            className={styles.item}
+            style={{ backgroundColor: getBgColor(colors[index]) }}
+            key={item.id}
+          >
             <span className={styles.label}>{item.label}</span>
             <span className={styles.percentage}>{item.percentage}%</span>
           </li>
